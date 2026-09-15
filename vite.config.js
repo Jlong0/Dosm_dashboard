f308784f73c5816a4281d41f08f570b1329c6689
Sky Tan <skycode112324@gmail.com>
2026-09-15 18:23:04 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Dosm_dashboard/",
   plugins: [react()],
   server: { port: 5173, strictPort: true },
   preview: { port: 4173, strictPort: true },
